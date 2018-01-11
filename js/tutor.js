7e51189e49f50864e5949db3c3d2e942c669c814
PDA <pokintelica@ukr.net>
2018-01-11 20:52:13 +0200

    ui-router tutorial done

--- js/tutor.js
+++ js/tutor.js
@@ -17,10 +17,76 @@ myApp.config(function ($stateProvider) {
         template: '<div ng-controller="aboutController"><h3 ng-bind="title"></h3></div>'
     }
 
-    $stateProvider.state(helloState).state(aboutState);
-   // $stateProvider.state(aboutState);
+    var peopleState = {
+        name:'people',
+        url: "/people",
+        resolve: {
+            people: function(PeopleService) {
+                return PeopleService.getPeople();
+            }
+        },
+        component: 'people'
+    }
+
+    var personState = {
+        name: 'people.person',
+        url: "/{personId}",
+        resolve: {
+            person: function(people, $stateParams) {
+                return people.find((person) => {
+                    return person.id === $stateParams.personId
+                });
+            }
+        },
+        component: 'person'
+    }
+
+    $stateProvider
+        .state(helloState)
+        .state(aboutState)
+        .state(peopleState)
+        .state(personState);
+
 });
+
+
 myApp.controller('aboutController',['$scope',function ($scope) {
     console.log('in controller')
     $scope.title = 'Its the UI-Router hello world app!';
+}]);
+
+
+myApp.component('people',{
+    bindings: {people: '<'},
+    templateUrl: "/tmpl/people.html",
+    controller: function () {
+        console.log('this.people',this.people)
+    }
+});
+
+myApp.component('person',{
+    bindings: {person: '<'},
+    templateUrl: "/tmpl/person.html"
+});
+
+
+myApp.service('PeopleService',['$http', function ($http) {
+   var service = {};
+
+   service.getPeople = function () {
+       return $http.get('people.json', {cache: true}).then((resp) => {
+           return resp.data
+       })
+   };
+
+   service.getPerson = function (personId) {
+     function getPersonById(person) {
+        return person.id === personId
+     }
+     return service.getPeople().then((persons) => {
+         return persons.find(getPersonById)
+     })
+   };
+
+   return service
 }]);
